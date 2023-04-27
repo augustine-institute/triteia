b539f6ac33267795e7d90b7d8bb92ede74f1ed3a
Alex Bourge <abourge@weare5stones.com>
2023-04-27 09:46:54 -0500

    feat(mariadb.service): load connection limit from env

--- src/database/mariadb/mariadb.service.ts
+++ src/database/mariadb/mariadb.service.ts
@@ -8,6 +8,7 @@ export class MariadbService
   extends DatabaseService
   implements BeforeApplicationShutdown {
   protected retryDelay = 20;
+  protected CONNECTION_LIMIT = process.env.DB_CONNECTION_LIMIT;
 
   /** A pool of db connections */
   private readonly pool = mariadb.createPool({
@@ -16,7 +17,7 @@ export class MariadbService
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    connectionLimit: 10,
+    connectionLimit: this.CONNECTION_LIMIT ? Number(this.CONNECTION_LIMIT) : 3,
     socketTimeout: 30000,
     connectTimeout: 30000,
   });
