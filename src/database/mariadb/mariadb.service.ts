dcd3eb2b8ac97eca474b7beea60e541ab489537f
Alex Bourge <abourge@weare5stones.com>
2023-04-27 10:50:52 -0500

    feat(mariadb.service): set connection default to 10

--- src/database/mariadb/mariadb.service.ts
+++ src/database/mariadb/mariadb.service.ts
@@ -17,7 +17,7 @@ export class MariadbService
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    connectionLimit: this.CONNECTION_LIMIT ? Number(this.CONNECTION_LIMIT) : 3,
+    connectionLimit: this.CONNECTION_LIMIT ? Number(this.CONNECTION_LIMIT) : 10,
     socketTimeout: 30000,
     connectTimeout: 30000,
   });
